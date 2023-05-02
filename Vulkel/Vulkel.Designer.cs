58d6ab3348868f526e51ba3a8f3ef5362ad89185
codernocook <90613458+codernocook@users.noreply.github.com>
2023-05-02 17:24:20 +0100

    First build

---
+++ Vulkel/Vulkel.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Vulkel
+{
+    partial class Vulkel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Vulkel));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.close_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.execute = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.Button();
+            this.inject = new System.Windows.Forms.Button();
+            this.scripthub = new System.Windows.Forms.Button();
+            this.executeFile = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(21)))));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Controls.Add(this.close_button);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.Location = new System.Drawing.Point(0, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(672, 23);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // close_button
+            // 
+            this.close_button.FlatAppearance.BorderSize = 0;
+            this.close_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close_button.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.close_button.Location = new System.Drawing.Point(647, 4);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(22, 20);
+            this.close_button.TabIndex = 0;
+            this.close_button.Text = "X";
+            this.close_button.UseVisualStyleBackColor = true;
+            this.close_button.Click += new System.EventHandler(this.close_button_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(625, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(4, 23);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(546, 274);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // execute
+            // 
+            this.execute.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(21)))));
+            this.execute.Cursor = System.Windows.Forms.Cursors.Default;
+            this.execute.FlatAppearance.BorderSize = 0;
+            this.execute.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.execute.ForeColor = System.Drawing.Color.Snow;
+            this.execute.Location = new System.Drawing.Point(12, 313);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(75, 24);
+            this.execute.TabIndex = 2;
+            this.execute.Text = "Execute";
+            this.execute.UseVisualStyleBackColor = false;
+            this.execute.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(21)))));
+            this.clear.Cursor = System.Windows.Forms.Cursors.Default;
+            this.clear.FlatAppearance.BorderSize = 0;
+            this.clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clear.ForeColor = System.Drawing.Color.Snow;
+            this.clear.Location = new System.Drawing.Point(93, 313);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 24);
+            this.clear.TabIndex = 3;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(21)))));
+            this.openFile.Cursor = System.Windows.Forms.Cursors.Default;
+            this.openFile.FlatAppearance.BorderSize = 0;
+            this.openFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openFile.ForeColor = System.Drawing.Color.Snow;
+            this.openFile.Location = new System.Drawing.Point(174, 313);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(75, 24);
+            this.openFile.TabIndex = 4;
+            this.openFile.Text = "Open File";
+            this.openFile.UseVisualStyleBackColor = false;
+            this.openFile.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // inject
+            // 
+            this.inject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(21)))));
+            this.inject.Cursor = System.Windows.Forms.Cursors.Default;
+            this.inject.FlatAppearance.BorderSize = 0;
+            this.inject.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inject.ForeColor = System.Drawing.Color.Snow;
+            this.inject.Location = new System.Drawing.Point(336, 313);
+            this.inject.Name = "inject";
+            this.inject.Size = new System.Drawing.Size(75, 24);
+            this.inject.TabIndex = 5;
+            this.inject.Text = "Inject";
+            this.inject.UseVisualStyleBackColor = false;
+            this.inject.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // scripthub
+            // 
+            this.scripthub.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(21)))));
+            this.scripthub.Cursor = System.Windows.Forms.Cursors.Default;
+            this.scripthub.FlatAppearance.BorderSize = 0;
+            this.scripthub.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scripthub.ForeColor = System.Drawing.Color.Snow;
+            this.scripthub.Location = new System.Drawing.Point(582, 313);
+            this.scripthub.Name = "scripthub";
+            this.scripthub.Size = new System.Drawing.Size(75, 24);
+            this.scripthub.TabIndex = 6;
+            this.scripthub.Text = "Script Hub";
+            this.scripthub.UseVisualStyleBackColor = false;
+            this.scripthub.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // executeFile
+            // 
+            this.executeFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(21)))));
+            this.executeFile.Cursor = System.Windows.Forms.Cursors.Default;
+            this.executeFile.FlatAppearance.BorderSize = 0;
+            this.executeFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.executeFile.ForeColor = System.Drawing.Color.Snow;
+            this.executeFile.Location = new System.Drawing.Point(255, 313);
+            this.executeFile.Name = "executeFile";
+            this.executeFile.Size = new System.Drawing.Size(75, 24);
+            this.executeFile.TabIndex = 7;
+            this.executeFile.Text = "Execute File";
+            this.executeFile.UseVisualStyleBackColor = false;
+            this.executeFile.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(21)))));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(556, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(101, 264);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Vulkel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(2)))), ((int)(((byte)(21)))));
+            this.ClientSize = new System.Drawing.Size(669, 349);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.executeFile);
+            this.Controls.Add(this.scripthub);
+            this.Controls.Add(this.inject);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.execute);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Snow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Vulkel";
+            this.Text = "Vulkel";
+            this.Load += new System.EventHandler(this.Vulkel_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button close_button;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Button inject;
+        private System.Windows.Forms.Button scripthub;
+        private System.Windows.Forms.Button executeFile;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Vulkel/Vulkel.cs
@@ -0,0 +1,246 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oxygen;
+
+namespace Vulkel
+{
+    public partial class Vulkel : Form
+    {
+        public Vulkel()
+        {
+            InitializeComponent();
+        }
+
+        WebClient wc = new WebClient();
+        private string defPath = Application.StartupPath + "//Monaco//";
+
+        private void addIntel(string label, string kind, string detail, string insertText)
+        {
+            string text = "\"" + label + "\"";
+            string text2 = "\"" + kind + "\"";
+            string text3 = "\"" + detail + "\"";
+            string text4 = "\"" + insertText + "\"";
+            webBrowser1.Document.InvokeScript("AddIntellisense", new object[]
+            {
+                label,
+                kind,
+                detail,
+                insertText
+            });
+        }
+
+        private void addGlobalF()
+        {
+            string[] array = File.ReadAllLines(this.defPath + "//globalf.txt");
+            foreach (string text in array)
+            {
+                bool flag = text.Contains(':');
+                if (flag)
+                {
+                    this.addIntel(text, "Function", text, text.Substring(1));
+                }
+                else
+                {
+                    this.addIntel(text, "Function", text, text);
+                }
+            }
+        }
+
+        private void addGlobalV()
+        {
+            foreach (string text in File.ReadLines(this.defPath + "//globalv.txt"))
+            {
+                this.addIntel(text, "Variable", text, text);
+            }
+        }
+
+        private void addGlobalNS()
+        {
+            foreach (string text in File.ReadLines(this.defPath + "//globalns.txt"))
+            {
+                this.addIntel(text, "Class", text, text);
+            }
+        }
+
+        private void addMath()
+        {
+            foreach (string text in File.ReadLines(this.defPath + "//classfunc.txt"))
+            {
+                this.addIntel(text, "Method", text, text);
+            }
+        }
+
+        private void addBase()
+        {
+            foreach (string text in File.ReadLines(this.defPath + "//base.txt"))
+            {
+                this.addIntel(text, "Keyword", text, text);
+            }
+        }
+
+        private async void Vulkel_Load(object sender, EventArgs e)
+        {
+            // Listbox Auto refresh
+            listBox1.Items.Clear();//Clear Items in the LuaScriptList
+            Functions.PopulateListBox(listBox1, "./Scripts", "*.txt");
+            Functions.PopulateListBox(listBox1, "./Scripts", "*.lua");
+
+            // Monaco
+            WebClient wc = new WebClient();
+            wc.Proxy = null;
+            try
+            {
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                string friendlyName = AppDomain.CurrentDomain.FriendlyName;
+                bool flag2 = registryKey.GetValue(friendlyName) == null;
+                if (flag2)
+                {
+                    registryKey.SetValue(friendlyName, 11001, RegistryValueKind.DWord);
+                }
+                registryKey = null;
+                friendlyName = null;
+            }
+            catch (Exception)
+            {
+            }
+            webBrowser1.Url = new Uri(string.Format("file:///{0}/Monaco/Monaco.html", Directory.GetCurrentDirectory()));
+            await Task.Delay(500);
+            webBrowser1.Document.InvokeScript("SetTheme", new string[]
+            {
+                   "Dark"
+            });
+            addBase();
+            addMath();
+            addGlobalNS();
+            addGlobalV();
+            addGlobalF();
+            webBrowser1.Document.InvokeScript("SetText", new object[]
+            {
+                 ""
+            });
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void close_button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private Point lastPoint;
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HtmlDocument document = webBrowser1.Document;
+            string scriptName = "GetText";
+            object[] args = new string[0];
+            object obj = document.InvokeScript(scriptName, args);
+            string script = obj.ToString();
+            Execution.Execute(script);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            API.Inject();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("SetText", new object[]
+            {
+                 ""
+            });
+            listBox1.Items.Clear(); //Clear Items in the LuaScriptList
+            Functions.PopulateListBox(listBox1, "./scripts", "*.txt");
+            Functions.PopulateListBox(listBox1, "./scripts", "*.lua");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog1.Title = "Open";
+                string readInfomation = File.ReadAllText(openFileDialog1.FileName).ToString();
+
+                webBrowser1.Document.InvokeScript("SetText", new object[]
+                {
+                     readInfomation
+                });
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog1.Title = "Open";
+                string readInfomation = File.ReadAllText(openFileDialog1.FileName).ToString();
+
+                Execution.Execute(readInfomation);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null) { return; }
+            webBrowser1.Document.InvokeScript("SetText", new object[]
+            {
+                 File.ReadAllText($"./scripts/{listBox1.SelectedItem}").ToString()
+            });
+        }
+    }
+}
